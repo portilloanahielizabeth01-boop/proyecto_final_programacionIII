3efcec483324c33c068c1603fac51db1f5f58aef
yasrojas14b-pixel <yasrojas14b@gmail.com>
2026-07-05 17:10:33 -0300

    stock

---
+++ app/Http/Controllers/StockController.php
@@ -0,0 +1,14 @@
+<?php
+namespace App\Http\Controllers;
+
+use App\Models\Stock;
+use Illuminate\Http\Request;
+
+class StockController extends Controller
+{
+    public function index()
+    {
+        $stock = Stock::all();
+        return view('stock.index', compact('stock'));
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,7 @@
 <?php
 
-use Illuminate\Support\Facades\Route;
-use App\Http\Controllers\ClienteController;
+
+use App\Http\Controllers\StockController;
 
 /*
 |--------------------------------------------------------------------------
@@ -27,13 +27,17 @@ Route::prefix('clientes')->group(function () {
     Route::delete('/{id}', [ClienteController::class, 'destroy']);  // Eliminar
 });
 
+use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\ClienteController;
 use App\Http\Controllers\AuthController;
-
 // Rutas de Autenticación
 Route::get('/login', [AuthController::class, 'showLogin'])->name('login');
 Route::post('/login', [AuthController::class, 'login']);
 Route::post('/logout', [AuthController::class, 'logout'])->name('logout');
-use App\Http\Controllers\RegistroController;
+// Rutas de Stock
+Route::get('/stock', [StockController::class, 'index'])->name('stock.index');
+
+
 
 // Rutas exclusivas para el registro de Anahí
 Route::get('/registro-tecnico', [RegistroController::class, 'mostrarFormulario'])->name('tecnico.registro.ver');
